b9431a2b45ad8eb8002ea1096bdc81ca3cff9df0
nlamonaco86 <nlamonaco86@gmail.com>
2020-09-18 21:24:24 -0400

    fixed win condition

--- script.js
+++ script.js
@@ -1,32 +1,40 @@
 const playGame = () => {
     // Create the random numbers for the powerball
     let compNum = []
-    for (let i = 1; i < 9; i++){
+    for (let i = 1; i < 9; i++) {
         compNum.push(Math.floor(Math.random() * 101))
     }
     console.log(compNum)
-    
+
     let userNum = []
-    for (let i = 1; i < 8; i++){
+    for (let i = 1; i < 8; i++) {
         userNum.push(parseInt(document.getElementById("guess" + [i]).value))
     }
     console.log(userNum)
 
     // Output each random number
-    for (let i = 1; i < 8; i++){
+    for (let i = 1; i < 8; i++) {
         document.getElementById("test" + [i]).innerHTML = (compNum[i])
     }
 
-    for (var i  = 0; i < 8; i++){
-        if (compNum[i] === userNum[i]){
+    let win = true;
+    let gotOne = false;
+    for (let i = 0; i < 8; i++) {
+        if (compNum[i] === userNum[i]) {
+            gotOne = true;
+        }
+        else if (compNum[i] != userNum[i]) {
+            win = false;
+        }
+    }
+    if (win) {
+        document.getElementById("winner").innerHTML = "You Won!!"
+    }
+    else if (gotOne) {
         document.getElementById("winner").innerHTML = "You got one!!"
-        }
-        else if (compNum === userNum ) {
-        document.getElementById("winner").innerHTML = "YOU WIN!!";
-        }
-        else {
-        document.getElementById("winner").innerHTML = "You Lose!!";    
-        }
+    }
+    else {
+        document.getElementById("winner").innerHTML = "You Lose!!"
     }
 }
 
